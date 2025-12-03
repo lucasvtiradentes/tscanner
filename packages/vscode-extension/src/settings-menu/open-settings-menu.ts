124c83651bf98d3329bcde97b14fe3d71a002cb5
Lucas Vieira <lucasvtiradentes@gmail.com>
2025-12-02 22:57:39 -0300

    refactor: reorganize menu items in open settings menu

--- packages/vscode-extension/src/settings-menu/open-settings-menu.ts
+++ packages/vscode-extension/src/settings-menu/open-settings-menu.ts
@@ -49,25 +49,23 @@ export function createOpenSettingsMenuCommand(ctx: CommandContext, panelContent:
     ];
 
     if (configState.hasAny) {
-      mainMenuItems.push({
-        id: SettingsMenuOption.ManageScanMode,
-        label: '$(gear) Manage Scan Mode',
-        detail: 'Choose between Codebase or Branch scan mode',
-      });
-    }
-
-    mainMenuItems.push({
-      id: SettingsMenuOption.ManageConfigLocation,
-      label: '$(folder) Manage Config Location',
-      detail: currentLocationLabel,
-    });
-
-    if (configState.hasAny) {
-      mainMenuItems.push({
-        id: SettingsMenuOption.OpenConfigFile,
-        label: '$(edit) Open Config File',
-        detail: 'Edit current config file',
-      });
+      mainMenuItems.push(
+        {
+          id: SettingsMenuOption.ManageScanMode,
+          label: '$(gear) Manage Scan Mode',
+          detail: 'Choose between Codebase or Branch scan mode',
+        },
+        {
+          id: SettingsMenuOption.ManageConfigLocation,
+          label: '$(folder) Manage Config Location',
+          detail: currentLocationLabel,
+        },
+        {
+          id: SettingsMenuOption.OpenConfigFile,
+          label: '$(edit) Open Config File',
+          detail: 'Edit current config file',
+        },
+      );
     }
 
     const selected = await vscode.window.showQuickPick(mainMenuItems, {
@@ -81,19 +79,15 @@ export function createOpenSettingsMenuCommand(ctx: CommandContext, panelContent:
 
     switch (selected.id) {
       case SettingsMenuOption.ManageRules:
-        logger.info('User selected: Manage Rules');
         await executeCommand(Command.ManageRules);
         break;
       case SettingsMenuOption.ManageScanMode:
-        logger.info('User selected: Manage Scan Mode');
         await showScanModeMenu(updateStatusBar, currentScanModeRef, currentCompareBranchRef, context, panelContent);
         break;
       case SettingsMenuOption.ManageConfigLocation:
-        logger.info('User selected: Manage Config Location');
         await showConfigLocationMenu(updateStatusBar, updateBadge, currentCustomConfigDirRef, context, panelContent);
         break;
       case SettingsMenuOption.OpenConfigFile:
-        logger.info('User selected: Open Config File');
         await openConfigFile(context, currentCustomConfigDirRef.current);
         break;
     }
