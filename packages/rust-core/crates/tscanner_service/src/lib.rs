6b5c55c1d9c01fe3ca8f45ba676ddac415b49f80
Lucas Vieira <lucasvtiradentes@gmail.com>
2025-12-09 23:29:09 -0300

    refactor: rust code

--- packages/rust-core/crates/tscanner_service/src/lib.rs
+++ packages/rust-core/crates/tscanner_service/src/lib.rs
@@ -1,12 +1,11 @@
 mod errors;
-mod logger;
 mod server;
 mod types;
 mod workspace;
 
 pub use errors::WorkspaceError;
-pub use logger::{get_logger, init_logger, log_debug, log_error, log_info, log_warn};
 pub use server::WorkspaceServer;
+pub use tscanner_logger::{get_logger, init_logger, log_debug, log_error, log_info, log_warn};
 pub use types::*;
 pub use workspace::Workspace;
 
